f63386e9ca6a88a7d16ebe99807da06881c7f600
Kang Hyungu <khgkjg12@naver.com>
2018-10-17 21:55:28 +0900

    객체 파일 컨샙잡기

--- graphic2d/src/main/java/com/khgkjg12/graphic2d/Object2D.java
+++ graphic2d/src/main/java/com/khgkjg12/graphic2d/Object2D.java
@@ -4,14 +4,33 @@ import android.graphics.Rect;
 
 public class Object2D {
 
-    Texture mTexture;
-    Rect mBoundary;
+    private Texture mTexture;
+    private Rect mBoundary;
+    private int x, y;
+    private int mWidth, mHeight;
+    private int mState;
+    public static final int NO_WHERE = 0;//데이터로써만 존제.
+    public static final int NORMAL = 1;//터치랑 충돌 모두 가능.
+    public static final int NO_COLLISION = 2;//충돌 불가.
+    public static final int NO_TOUCH = 3;//터치 불가.
 
-    public Object2D(Texture texture, Rect boundary) {
+    public Object2D(Texture texture, int width, int height) {
         mTexture = texture;
-        mBoundary = boundary;
+        mWidth = width;
+        mHeight = height;
+        mBoundary = new Rect();
+        mState = NO_WHERE;
     }
 
+    //객체의 좌표를 설정.
+    public void setPosition(int x, int y){
+        mBoundary.set(x-mWidth/2, y-mHeight/2, x+mWidth/2, y+mHeight/2);
+    }
+
+    //객체를 물리적 상태를 변화.
+    public void setState(int state){
+        mState = state;
+    }
 
     public void dispose(){
         mTexture.dispose();
